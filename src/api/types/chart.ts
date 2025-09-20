656b557662ff18a15bb2ef97e784874b09115d01
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 22:35:07 -0600

    crear tipos de datos  los esquemas de la api

---
+++ src/api/types/chart.ts
@@ -0,0 +1,28 @@
+export interface ChartParameter {
+    x_axis: string
+    y_axis: string
+}
+
+export interface ChartSchema {
+    title: string
+    chart_type: string
+    parameter: ChartParameter[]
+}
+
+
+export interface DataValue {
+    x: string
+    y: string
+}
+
+
+export interface ChartParameterProcessed extends ChartParameter{
+    data: DataValue[]
+}
+
+
+export interface Chart{
+    title: string
+    chart_type: string
+    data: ChartParameterProcessed[]
+}
