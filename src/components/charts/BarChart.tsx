ef09305ff51b3bfd6b44a76d004cef9407fe7bd3
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 23:47:27 -0600

    actualizar diseños

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -51,7 +51,7 @@ export function BarChartComponent({ data, title, xAxisLabel, yAxisLabel }: BarCh
   return (
     <div className="w-full p-4">
       <h3 className="text-lg font-semibold mb-4 text-foreground">{title}</h3>
-      <ChartContainer config={chartConfig} className="h-full w-full pb-5">
+      <ChartContainer config={chartConfig} className="h-full w-full pb-2">
         <BarChart data={currentItems}>
           <CartesianGrid vertical={false} />
           <XAxis
