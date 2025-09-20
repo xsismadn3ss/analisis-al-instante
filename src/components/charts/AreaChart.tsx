54cff382c359942d4fec118473f8933ca1d3bf69
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-19 23:46:50 -0600

    actaulizar estilos

--- src/components/charts/AreaChart.tsx
+++ src/components/charts/AreaChart.tsx
@@ -51,7 +51,7 @@ export function AreaChartComponent({ data, title, xAxisLabel, yAxisLabel }: Area
   return (
     <div className="w-full p-4">
       <h3 className="text-lg font-semibold mb-4 text-foreground">{title}</h3>
-      <ChartContainer config={chartConfig} className="h-full w-full pb-10">
+      <ChartContainer config={chartConfig} className="h-full w-full pb-2">
         <AreaChart data={currentItems}>
           <CartesianGrid vertical={false} />
           <XAxis
